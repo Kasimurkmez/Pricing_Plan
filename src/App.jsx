32cc16bb3c7223abbf7def9e60860524c99aca9a
kasimurkmez <kasimurkmez47@gmail.com>
2023-07-03 12:02:40 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import "./App.css";
+import PricingCard from "./components/PricingCard";
+import "./styles/PricingApp.css";
 
 function App() {
   return (
-    <>
-      <h2>Hello</h2>
-    </>
+    <div className="PricingApp">
+      <div className="app-container">
+        <header>
+          <h1 className="header-topic">Our Pricing Plan</h1>
+          <div className="header-row">
+            <p>Annually</p>
+            <label className="price-switch">
+              <input type="checkbox" className="price-checkbox" />
+              <div className="switch-slider"></div>
+            </label>
+            <p>Monthly</p>
+          </div>
+        </header>
+        <div className="pricing-cards">
+          <PricingCard />
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/PricingCard.css";
+const PricingCard = () => {
+  return (
+    <div className="PricingCard">
+      <h1>Card</h1>
+    </div>
+  );
+};
+
+export default PricingCard;
