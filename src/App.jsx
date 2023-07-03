f789a50a943425f86e22845756f081875b84af0e
kasimurkmez <kasimurkmez47@gmail.com>
2023-07-03 13:14:05 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import PricingCard from "./components/PricingCard";
 import "./styles/PricingApp.css";
 
 function App() {
+  const [selectMontly, setSelectMonthly] = useState(true);
+
+  console.log(selectMontly);
   return (
     <div className="PricingApp">
       <div className="app-container">
@@ -10,14 +14,40 @@ function App() {
           <div className="header-row">
             <p>Annually</p>
             <label className="price-switch">
-              <input type="checkbox" className="price-checkbox" />
+              <input
+                type="checkbox"
+                className="price-checkbox"
+                onChange={() => {
+                  setSelectMonthly((prev) => !prev);
+                }}
+              />
               <div className="switch-slider"></div>
             </label>
             <p>Monthly</p>
           </div>
         </header>
         <div className="pricing-cards">
-          <PricingCard />
+          <PricingCard
+            title="Essential"
+            price={selectMontly ? "20.99" : "188.9"}
+            storage="50 GB Storage"
+            users="5"
+            sendUp="5"
+          />
+          <PricingCard
+            title="Deluxy"
+            price={selectMontly ? "34.99" : "349.9"}
+            storage="70 GB Storage"
+            users="10"
+            sendUp="10"
+          />
+          <PricingCard
+            title="Premium"
+            price={selectMontly ? "56.99" : "459.9"}
+            storage="100 GB Storage"
+            users="15"
+            sendUp="15"
+          />
         </div>
       </div>
     </div>
--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "../styles/PricingCard.css";
-const PricingCard = () => {
+const PricingCard = ({ title, price, storage, users, sendUp }) => {
   return (
     <div className="PricingCard">
-      <h1>Card</h1>
+      <header>
+        <p className="card-title">{title}</p>
+        <h1 className="card-price">{price}</h1>
+      </header>
+      <div className="card-features">
+        <div className="card-storage">{storage}</div>
+        <div className="card-users-allowed">{users} users in total </div>
+        <div className="card-send-up"> Send up to {sendUp}</div>
+      </div>
+      <button className="card-btn">READ MORE</button>
     </div>
   );
 };
